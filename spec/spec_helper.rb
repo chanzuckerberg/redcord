e274e14424f67280561e767783b75a50fbfdc4da
Donald Dong <ddong@chanzuckerberg.com>
2020-06-19 19:25:14 -0700

    Install simplecov and codecov
    Pull Request Branch: donaldong/install_simplecov_and_codecov
    Pull Request Link: https://github.com/chanzuckerberg/redis-record/pull/20

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 require 'redis_record'
+require 'simplecov'
+
+SimpleCov.start
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 Time.zone = 'UTC'
 
