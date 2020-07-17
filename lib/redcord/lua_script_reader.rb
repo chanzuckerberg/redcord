e4d85c35f55aa6e80a5153b0a4ac3afbd8d07e4f
Donald Dong <ddong@chanzuckerberg.com>
2020-07-17 14:31:57 -0700

    [fix] Require erb in lua_script_reader (#47)
    This fixes an error when loading the redcord gem, as well as some rubocop lint errors.
    
    Pull Request Link: https://github.com/chanzuckerberg/redcord/pull/47
    Pull Request Branch: donaldong/fix_require_erb_in_lua_script_reader

--- lib/redcord/lua_script_reader.rb
+++ lib/redcord/lua_script_reader.rb
@@ -1,16 +1,27 @@
+# frozen_string_literal: true
+
 # typed: strict
+
+require 'erb'
+
 module Redcord::LuaScriptReader
   extend T::Sig
 
-  sig {params(script_name: String).returns(String) }
+  sig { params(script_name: String).returns(String) }
   def self.read_lua_script(script_name)
-    path = File.join(File.dirname(__FILE__), "server_scripts/#{script_name}.erb.lua")
+    path = File.join(
+      File.dirname(__FILE__),
+      "server_scripts/#{script_name}.erb.lua",
+    )
     ERB.new(File.read(path)).result(binding)
   end
 
-  sig {params(relative_path: String).returns(String) }
+  sig { params(relative_path: String).returns(String) }
   def self.include_lua(relative_path)
-    path = File.join(File.dirname(__FILE__), "server_scripts/#{relative_path}.erb.lua")
+    path = File.join(
+      File.dirname(__FILE__),
+      "server_scripts/#{relative_path}.erb.lua",
+    )
     File.read(path)
   end
 end
