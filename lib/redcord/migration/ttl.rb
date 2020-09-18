35276c714d3f034f56c080bebf48fc2272886035
Donald Dong <ddong@chanzuckerberg.com>
2020-09-18 15:51:51 -0700

    [feature] Migration#change_ttl_active (#62)
    This adds a method allow us to change ttl for all existing records.
    
    ### Test Plan
    - [x] Added a test case
    
    Pull Request Link: https://github.com/chanzuckerberg/redcord/pull/62
    Pull Request Branch: donaldong/feature_migration_change_ttl_active

--- lib/redcord/migration/ttl.rb
+++ lib/redcord/migration/ttl.rb
@@ -2,10 +2,22 @@
 module Redcord::Migration::TTL
   extend T::Sig
 
+  sig { params(model: T.class_of(Redcord::Base)).returns(T.untyped) }
+  def _get_ttl(model)
+    model.class_variable_get(:@@ttl) || -1
+  end
+
   # This won't change ttl until we call update on a record
   sig { params(model: T.class_of(Redcord::Base)).void }
   def change_ttl_passive(model)
-    ttl = model.class_variable_get(:@@ttl)
-    model.redis.set("#{model.model_key}:ttl", ttl ? ttl : -1)
+    model.redis.set("#{model.model_key}:ttl", _get_ttl(model))
+  end
+
+  sig { params(model: T.class_of(Redcord::Base)).void }
+  def change_ttl_active(model)
+    change_ttl_passive(model)
+    model.redis.scan_each(match: "#{model.model_key}:id:*") do |key|
+      model.redis.expire(key, _get_ttl(model))
+    end
   end
 end
