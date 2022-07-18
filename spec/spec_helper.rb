23738fb6144b94dc457d6d37dda3e54f1df2add4
Matt Wang <matthew.wang@chanzuckerberg.com>
2022-07-18 13:24:00 -0700

    Replaces deprecated `codecov` gem with GitHub Action (#106)
    This PR:
    
    - removes `codecov` from the gemspec, `spec_helper.rb`
    - adds `simplecov-cobertura` (required for XML formatting)
    - uses the `codecov` action instead in `ci.yml`
    
    Closes #105.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@ require 'active_support/time'
 SimpleCov.start
 
 if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
 
 def cluster_mode?
